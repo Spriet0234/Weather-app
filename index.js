216c995186e1a37a563072bb642700b18159676b
Santiago P <spriet0234@gmail.com>
2023-06-17 21:43:36 -0400

    updates

--- index.js
+++ index.js
@@ -6,7 +6,36 @@ const cityText = document.getElementById("city");
 const image = document.getElementById("image");
 const condition = document.getElementById("condition");
 
-console.log(btn);
+const successCallback = (position) => {
+  console.log(position);
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  fetchApi(`${position.coords.latitude},${position.coords.longitude}`);
+};
+
+const errCallback = (error) => {
+  console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+function errorCallback(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      console.log("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log("An unknown error occurred.");
+      break;
+  }
+}
+
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     fetchApi(input.value);
